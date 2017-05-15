0fd2c4b9380fe40df0546356bd922e34b47dbe30
Bob van der Valk <info@mawoo.nl>
2017-05-15 21:28:11 +0200

    Add markers

--- app/src/main/java/a2/avans/nl/openstreetmapdemo/MainMap.java
+++ app/src/main/java/a2/avans/nl/openstreetmapdemo/MainMap.java
@@ -12,7 +12,13 @@ import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
 import org.osmdroid.views.overlay.MinimapOverlay;
+import org.osmdroid.views.overlay.OverlayItem;
+import org.osmdroid.views.overlay.gestures.RotationGestureOverlay;
+
+import java.util.ArrayList;
 
 public class MainMap extends AppCompatActivity {
     private MapView mapView;
@@ -37,11 +43,41 @@ public class MainMap extends AppCompatActivity {
 
     private void customMapSettings() {
         IMapController mapController = mapView.getController();
-        mapController.setZoom(19);
+        mapController.setZoom(19); // zoom level
+        // locatie instellen
         GeoPoint geoPoint = new GeoPoint(51.827373, 4.483371); // emulator testing
         //deze gebruiken als je een echt android apparaat hebt.
         //Location location = new Location("");
         //GeoPoint geoPoint = new GeoPoint(location.getLatitude(), location.getLongitude());
         mapController.setCenter(geoPoint);
+
+        // vinger rotatie accepteren
+        RotationGestureOverlay mRotationGestureOverlay = new RotationGestureOverlay(context, mapView);
+        mRotationGestureOverlay.setEnabled(true);
+        mapView.setMultiTouchControls(true);
+        mapView.getOverlays().add(mRotationGestureOverlay);
+        this.setMarkers();
+    }
+
+    private void setMarkers() {
+        MarkerList markerList = new MarkerList();
+        markerList.prefillMarkerList();
+        ArrayList<OverlayItem> items = markerList.getMarkerList();
+        ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<OverlayItem>(
+                this, items,
+                new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
+                    @Override
+                    public boolean onItemSingleTapUp(final int index, final OverlayItem item) {
+                        //do something
+                        return true;
+                    }
+                    @Override
+                    public boolean onItemLongPress(final int index, final OverlayItem item) {
+                        return false;
+                    }
+                });  // <----- removed the mResourceProxy parameter
+        mOverlay.setFocusItemsOnTap(true);
+
+        mapView.getOverlays().add(mOverlay);
     }
 }
